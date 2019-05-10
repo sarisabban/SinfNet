d7a0ad51e60c9a5854e28b2e1ed01603f5ef204e
Sari Sabban <sari.sabban@gmail.com>
2019-05-10 17:54:13 +0300

    Update Label.py

--- Label.py
+++ Label.py
@@ -552,10 +552,10 @@ def BOX(BBOX_line1, BBOX_line2):
 	# Define box2
 	line2 = BBOX_line2
 	line2 = line2.strip().split()
-	xmin2 =int(line1[0])
-	ymin2 =int(line1[1])
-	xmax2 =int(line1[2])
-	ymax2 =int(line1[3])
+	xmin2 =int(line2[0])
+	ymin2 =int(line2[1])
+	xmax2 =int(line2[2])
+	ymax2 =int(line2[3])
 	label2= line2[4]
 	# Calculate IOU
 	bb1 = {'x1':xmin1, 'x2':xmax1, 'y1':ymin1, 'y2':ymax1}
@@ -564,15 +564,17 @@ def BOX(BBOX_line1, BBOX_line2):
 	y_top = max(bb1['y1'], bb2['y1'])
 	x_right = min(bb1['x2'], bb2['x2'])
 	y_bottom = min(bb1['y2'], bb2['y2'])
-	if x_right < x_left or y_bottom < y_top: return 0.0
+	if x_right < x_left or y_bottom < y_top: return(False)
 	intersection_area = (x_right - x_left) * (y_bottom - y_top)
 	bb1_area = (bb1['x2'] - bb1['x1']) * (bb1['y2'] - bb1['y1'])
 	bb2_area = (bb2['x2'] - bb2['x1']) * (bb2['y2'] - bb2['y1'])
 	IOU = round((intersection_area / float(bb1_area + bb2_area - intersection_area))*100, 3)
 	if IOU > 50 and label1 == label2:
-		return(True)
+		print(IOU, label1, label2, 'True')
+#		return(True)
 	else:
-		return(False)
+		print(IOU, label1, label2, 'False')
+#		return(False)
 
 def eval(dir_test, dir_pred):
 	'''
@@ -584,10 +586,9 @@ def eval(dir_test, dir_pred):
 		next(FileT)
 		FileP = open('{}/{}'.format(dir_pred, fP), 'r')
 		next(FileP)
-
 		for lineT in FileT:
 			for lineP in FileP:
-				print(BOX(lineT, lineP))
+				BOX(lineT, lineP)
 
 
 
