d9944f9fce17210d533c45bc3e33688b59c08ef9
acresearch <sari.sabban@gmail.com>
2020-04-23 01:52:29 +0300

    added aknowlegenemnt lisense

--- sources/Augment.py
+++ sources/Augment.py
@@ -219,6 +219,31 @@ def augment_bbox(image_input='./dataset/Train',
 			print('Completed: {} {}'.format(Ioutput, Boutput))
 
 def augment_poly(TheImage, im_out, ann_path, ann_output, iterations):
+	'''
+	MIT License
+
+	Copyright (c) 2019 Seth Adams
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+	Adapted from: https://github.com/seth814/Semantic-Shapes
+	'''
 	''' Augment images with polygons and saves them into a new directory '''
 	try:
 		for iters in range(iterations):
